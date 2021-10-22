f134ea0e6012ad95185c10e7de8a03bafb947508
Sumedha Singh <41679173+sumedhamona@users.noreply.github.com>
2021-10-22 18:39:37 +0530

    input output processing

--- app.js
+++ app.js
@@ -9,10 +9,17 @@
 
 var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
+var outputDiv = document.querySelector("#output");
+
+// outputDiv.innerText = "Sumedha Singh"
 
 function clickEventHandler(){
     console.log("clicked");
     console.log("input", txtInput.value);
+    outputDiv.innerText = "cghytsdfrfvh" + txtInput.value;
 }
 
 btnTranslate.addEventListener("click", clickEventHandler)
+
+//emojipedia
+
