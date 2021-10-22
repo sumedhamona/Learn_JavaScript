c596438923157342567d0f02d758bef26cf8220d
Sumedha Singh <41679173+sumedhamona@users.noreply.github.com>
2021-10-22 19:45:20 +0530

    minion translation

--- app.js
+++ app.js
@@ -11,15 +11,40 @@ var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
 var outputDiv = document.querySelector("#output");
 
+//var serverURL = "https://lessonforapi.tanaypratap.repl.co/translate/yoda.json"
+
+var serverURL = "https://api.funtranslations.com/translate/minion.json"
 // outputDiv.innerText = "Sumedha Singh"
 
-function clickEventHandler(){
+function getTranslationURL(text) {
+    return serverURL + "?" + "text=" + text
+}
+
+function errorHandler(error) {
+    console.log("Error Occured", error);
+    alert("Something went wrong.");
+}
+
+function clickEventHandler() {
     console.log("clicked");
     console.log("input", txtInput.value);
     outputDiv.innerText = "cghytsdfrfvh" + txtInput.value;
-}
+
+    var inputText = txtInput.value; //taking input
+
+    //calling server for processing
+    fetch(getTranslationURL(inputText))
+        .then(response => response.json())
+        .then(json => {
+            var translatedText = json.contents.translated;
+            outputDiv.innerText = translatedText;
+        })
+        // .then(json => console.log(json.contents.translated))
+        .catch(errorHandler)
+};
 
 btnTranslate.addEventListener("click", clickEventHandler)
 
 //emojipedia
+//fun translations
 
