a65465778ce91bbf1c14fb20e8374814f32b9f7e
Sumedha Singh <41679173+sumedhamona@users.noreply.github.com>
2021-10-22 15:40:42 +0530

    querySelector and eventListener

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
-var username = prompt("Give me username?")
-var welcome = "This script works!!" + username;
-alert(welcome);
+//input
+// var username = prompt("Give me username?")
+
+//processing
+// var welcome = "This script works!!" + username;
+
+//output
+// alert(welcome);
+
+var btnTranslate = document.querySelector("#btn-translate");
+
+btnTranslate.addEventListener("click", function clickEventHandler(){
+    console.log("clicked");
+})
